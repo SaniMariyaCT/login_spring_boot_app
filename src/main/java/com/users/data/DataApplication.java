ae43a3e72349677b71aa3a8d7a150da41d08f545
C T <765975@cognizant.com>
2019-03-13 16:43:49 +0530

    First commit

---
+++ src/main/java/com/users/data/DataApplication.java
@@ -0,0 +1,13 @@
+package com.users.data;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataApplication {
+
+    public static void main(String[] args) throws Throwable {
+        SpringApplication.run(DataApplication.class, args);
+    }
+
+}
